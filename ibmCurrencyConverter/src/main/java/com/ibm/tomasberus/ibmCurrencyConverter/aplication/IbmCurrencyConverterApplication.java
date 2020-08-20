5a17e2bc45b9d0ab2b482c115bf44a5ac0ff47cc
TomasBerus <tomasberus@gmail.com>
2020-08-20 09:25:22 +0300

    Fixes for the CC

---
+++ ibmCurrencyConverter/src/main/java/com/ibm/tomasberus/ibmCurrencyConverter/aplication/IbmCurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.tomasberus.ibmCurrencyConverter.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmCurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmCurrencyConverterApplication.class, args);
+	}
+
+}
---
+++ ibmCurrencyConverter/src/test/java/com/ibm/tomasberus/ibmCurrencyConverter/IbmCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.tomasberus.ibmCurrencyConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmCurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
